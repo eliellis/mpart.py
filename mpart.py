6c480a09e4126fe8d69ada75e8ed3dfef1644b51
Eli Ellis <me@eliellis.com>
2012-07-27 03:15:14 -0400

    Now only requires one dictionary which improved flexibility.

--- mpart.py
+++ mpart.py
@@ -1,23 +1,25 @@
 import random, mimetypes, string
 
-def encode(fields={}, files={}):
+def encode(fields={}):
 	random_boundary = __randy_boundary()
 	content_type = "multipart/form-data, boundary=%s" % random_boundary
 
 	form_data = []
+	
 	if fields:
 		for (key, value) in fields.iteritems():
-			itemstr = '--%s\r\nContent-Disposition: form-data; name="%s"\r\n\r\n%s\r\n' % (random_boundary, key, value)
-			form_data.append(itemstr)
+			if not hasattr(value, 'read'):
+				itemstr = '--%s\r\nContent-Disposition: form-data; name="%s"\r\n\r\n%s\r\n' % (random_boundary, key, value)
+				form_data.append(itemstr)
+			elif hasattr(value, 'read'):
+				with value:
+					file_mimetype = mimetypes.guess_type(value.name)[0] if mimetypes.guess_type(value.name)[0] else 'application/octet-stream'
+					itemstr = '--%s\r\nContent-Disposition: form-data; name="%s"; filename="%s"\r\nContent-Type: %s\r\n\r\n%s\r\n' % (random_boundary, key, value.name, file_mimetype, value.read())
+				form_data.append(itemstr)
+			else:
+				raise Exception(value, 'Field is neither a file handle or any other decodable type.')
 	else:
 		pass
-
-	if files:
-		for (key, value) in files.iteritems():
-			with value:
-				file_mimetype = mimetypes.guess_type(value.name)[0] if mimetypes.guess_type(value.name)[0] else 'application/octet-stream'
-				itemstr = '--%s\r\nContent-Disposition: form-data; name="%s"; filename="%s"\r\nContent-Type: %s\r\n\r\n%s\r\n' % (random_boundary, key, value.name, file_mimetype, value.read())
-			form_data.append(itemstr)
 
 	form_data.append('--%s--\r\n' % random_boundary)
 
@@ -34,3 +36,6 @@ def __randy_boundary(length=10,reshuffle=False):
 	else:
 		pass
 	return ''.join(boundary_string)
+
+data = {'nig':'nog','fuck':'yes please','file':open('README.md')}
+print encode(data)[1]
