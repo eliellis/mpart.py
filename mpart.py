8e4aae488729d2379c42114abaf72e82c6ee83b4
Eli Ellis <me@eliellis.com>
2012-07-26 21:07:54 -0400

    _Everything_ working; the encoding is much more lax on fields/files having to be specified.
    *Amended the encoding function to check to see if the has tables are
    empty before executing the code blocks.
    
    *Privatized the random boundary function
    
    *Changed the name of the encoding function to 'encode'
    
    *Doesn't throw an exceptions when the fields/files variable isn't
    specified

--- mpart.py
+++ mpart.py
@@ -1,25 +1,29 @@
-import random
+import random, mimetypes
 
-def mpart_encode(fields, files):
-	random_boundary = randy_boundary(10)
+def encode(fields={}, files={}):
+	random_boundary = __randy_boundary()
 	content_type = "multipart/form-data, boundary=%s" % random_boundary
 
-
 	form_data = []
-	for (key, value) in fields.iteritems():
-		itemstr = '--%s\r\nContent-Disposition: form-data; name="%s"\r\n\r\n%s\r\n' % (random_boundary, key, value)
-		form_data.append(itemstr)
+	if fields:
+		for (key, value) in fields.iteritems():
+			itemstr = '--%s\r\nContent-Disposition: form-data; name="%s"\r\n\r\n%s\r\n' % (random_boundary, key, value)
+			form_data.append(itemstr)
+	else:
+		pass
 
-	for (key, value) in files.iteritems():
-		with value:
-			itemstr = '--%s\r\nContent-Disposition: form-data; name="%s"; filename="%s"\r\n\r\n%s\r\n' % (random_boundary, key, value.name, value.read())
-		form_data.append(itemstr)
+	if files:
+		for (key, value) in files.iteritems():
+			with value:
+				file_mimetype = mimetypes.guess_type(value.name)[0] if mimetypes.guess_type(value.name)[0] else 'application/octet-stream'
+				itemstr = '--%s\r\nContent-Disposition: form-data; name="%s"; filename="%s"\r\nContent-Type: %s\r\n\r\n%s\r\n' % (random_boundary, key, value.name, file_mimetype, value.read())
+			form_data.append(itemstr)
 
 	form_data.append('--%s--\r\n' % random_boundary)
 
 	return content_type, ''.join(form_data)
 
-def randy_boundary(length=5,reshuffle=False):
+def __randy_boundary(length=10,reshuffle=False):
 	character_string = "abcdefghijklmnopqrstuvwxz1234567890"
 	boundary_string = []
 	for i in range(0,length):
